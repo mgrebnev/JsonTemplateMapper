80b3cbb5c302414d4556d2d445e2414ef76c66ed
Максим Гребнев <solightingstats@yandex.ru>
2018-09-06 23:01:38 +0300

    gitignore blet + refactoring

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,9 +4,6 @@ import com.example.models.Lawsuit;
 import com.example.utils.JsonTemplateMapper;
 
 import java.io.File;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -16,48 +13,9 @@ public class Main {
         lawsuit.setAccountUsername("vladimirmihailovich");
         lawsuit.setContractNumber("4762992839283982");
 
-        File file = new File("example.txt");
-        JsonTemplateMapper.map(file,lawsuit).forEach(System.out::println);
-    }
-
-    //TODO remove
-    private static String getTextBetweenTwoParts(String text, String startPart, String secondPart){
-        if (text != null){
-            Integer startSymbolPosition = text.indexOf(startPart);
-            Integer endSymbolPosition = text.indexOf(secondPart);
-            return text.substring(startSymbolPosition + startPart.length(), endSymbolPosition);
-        }else {
-            return text;
-        }
-    }
-
-    private static Map<String, Object> getMethodsWithReturnValuesByClass(Object object, Class clazz) throws Exception{
-        Map<String,Object> methods = new HashMap<>();
-        if (clazz != null){
-            Method[] clazzMethods = clazz.getMethods();
-            for (Method currentMethod: clazzMethods){
-                String currentMethodName = currentMethod.getName();
-                if (currentMethodName.startsWith("get") && !currentMethod.getReturnType().equals(Class.class)) {
-                    String adaptedMethodName = adaptMethodName(currentMethodName);
-                    Object methodReturnValue = currentMethod.invoke(object);
-                    methods.put(adaptedMethodName, methodReturnValue);
-                }
-            }
-        }
-        return methods;
-    }
-
-    private static String adaptMethodName(String methodName){
-        String adaptedMethodName = removeExpressionFromString(methodName, "get");
-        String firstSymbolInLowerCase = toLowerCase(adaptedMethodName, 1);
-        return firstSymbolInLowerCase + adaptedMethodName.substring(1, adaptedMethodName.length());
-    }
-
-    private static String removeExpressionFromString(String string, String expression){
-        return string.replaceAll(expression, "");
-    }
-
-    private static String toLowerCase(String string, int symbolIndex){
-        return string.substring(symbolIndex - 1, symbolIndex).toLowerCase();
+        /*File file = new File("example.txt");
+        JsonTemplateMapper.map(file,lawsuit).forEach(System.out::println);*/
+        Integer length = "Hello, World\n Hello\n Petr".split("\n").length;
+        System.out.println(length);
     }
 }
--- src/main/java/com/example/utils/JsonTemplateMapper.java
+++ src/main/java/com/example/utils/JsonTemplateMapper.java
@@ -8,20 +8,27 @@ import java.util.List;
 import java.util.Map;
 
 public class JsonTemplateMapper {
-    private static final String lineParseRegex = "['$']['('](\\d+|\\w+|(['*']|[')']|['(']|['-']|['_']|[';']))+[')']";
-
+    private static final String lineParseRegex = "['$']['('](\\d+|\\w+|(|[')']|['(']|['-']|['_']|))+[')']";
+    
+    private static final String lSeparator = "$(";
+    private static final String rSeparator  =  ")"; 
+    
     private JsonTemplateMapper() {
     }
 
     // TODO refactor
     public static List<String> map(File jsonTemplate, Object flatEntity) throws Exception {
-        Map<String, Object> clazzMethods = getMethodsWithReturnValuesByClass(flatEntity);
-        List<String> templateLines = getLines(jsonTemplate);
+        Map<String, Object> clazzMethods = getMethodsWithReturnValuesByObject(flatEntity);
+        List<String> templateLines = readAllLines(jsonTemplate);
         for (int i = 0; i < templateLines.size(); i++){
             String currentLine = templateLines.get(i);
-            for (Map.Entry entry: clazzMethods.entrySet()){
-                if (currentLine.contains("$(" + entry.getKey() + ")")){
-                    String modifiedLine = currentLine.replaceAll(lineParseRegex, "\"" + entry.getValue() + "\"");
+            for (Map.Entry<String,Object> clazzMethod: clazzMethods.entrySet()){
+                String currentRefundValue = (String) clazzMethod.getValue();
+                String wrappedField = lSeparator + currentRefundValue + rSeparator;
+                if (isStringContainsWrappedField(currentLine,wrappedField)){
+                    String modifiedLine = currentLine.replaceAll(
+                            lineParseRegex, "\"" + clazzMethod.getValue() + "\""
+                    );
                     templateLines.set(i, modifiedLine);
                     break;
                 }
@@ -31,30 +38,27 @@ public class JsonTemplateMapper {
     }
 
 
-    private static Map<String, Object> getMethodsWithReturnValuesByClass(Object object) throws Exception{
+    private static Map<String, Object> getMethodsWithReturnValuesByObject(Object object) throws Exception{
         Map<String,Object> methods = new HashMap<>();
-        Class currentClazz = object.getClass();
-        if (currentClazz != null){
+        if (object != null){
+            Class currentClazz = object.getClass();
             Method[] clazzMethods = currentClazz.getMethods();
             for (Method currentMethod: clazzMethods){
                 if (isPOJOGetter(currentMethod)) {
                     String adaptedMethodName = adaptMethodName(currentMethod.getName());
-                    Object methodReturnValue = currentMethod.invoke(object);
-                    methods.put(adaptedMethodName, methodReturnValue);
+                    Object refundValue = currentMethod.invoke(object);
+                    methods.put(adaptedMethodName, refundValue);
                 }
             }
         }
         return methods;
     }
-
-    private static List<String> getLines(File file){
-        try {
-            return Files.readAllLines(file.toPath());
-        }catch (Exception ex){
-            throw new RuntimeException(ex);
-        }
+    
+    // TODO need efficient way
+    private static boolean isStringContainsWrappedField(String string, String wrappedField){
+        return string.contains(wrappedField);
     }
-
+    
     private static boolean isPOJOGetter(Method method){
         return method.getName().startsWith("get") && !method.getReturnType().equals(Class.class);
     }
@@ -71,5 +75,13 @@ public class JsonTemplateMapper {
 
     private static String toLowerCase(String string, int symbolIndex){
         return string.substring(symbolIndex - 1, symbolIndex).toLowerCase();
+    }
+
+    private static List<String> readAllLines(File file){
+        try {
+            return Files.readAllLines(file.toPath());
+        }catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
     }
 }
