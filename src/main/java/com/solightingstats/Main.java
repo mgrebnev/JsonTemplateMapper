ee8d3348519b435249bb63611abdd21430202c21
Максим Гребнев <solightingstats@yandex.ru>
2018-09-08 13:46:58 +0300

    Fix bug with excess newline characters

--- src/main/java/com/solightingstats/Main.java
+++ src/main/java/com/solightingstats/Main.java
@@ -2,8 +2,8 @@ package com.solightingstats;
 
 import com.solightingstats.mapper.TemplateMapper;
 import com.solightingstats.mapper.converter.ValueConverter;
-import com.solightingstats.mapper.impl.JsonTemplateMapper;
 import com.solightingstats.mapper.converter.impl.StandardValueConverter;
+import com.solightingstats.mapper.impl.JsonTemplateMapper;
 import com.solightingstats.model.Lawsuit;
 
 import java.io.File;
@@ -17,11 +17,9 @@ public class Main {
         lawsuit.setContractNumber(null);
         lawsuit.setAccountUsername("vladimirmihailovich");
         File file = getFileFromResources("files/example.json");
-        
         ValueConverter valueConverter = new StandardValueConverter();
         TemplateMapper mapper = new JsonTemplateMapper(valueConverter);
         mapper.toJson(file,lawsuit).forEach(System.out::println);
-        
     }
     
     private static File getFileFromResources(String path) throws URISyntaxException {
